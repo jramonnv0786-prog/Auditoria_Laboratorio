be69292cdc523b797d3d91ac44fd6be99d99adea
Neira <jramonnv0786@gmail.com>
2026-09-24 12:39:51 +0200

    Creados los CASOS A Y B

--- ScriptinWEB/script3.js
+++ ScriptinWEB/script3.js
@@ -1,4 +1,5 @@
 for (let i = 0; i < 5000; i++) {
-    console.log(i);
+console.log(i);
+
 }
-document.getElementById("titulo").innerText = "Cambio por script3.js";
+document.getElementById('titulo').innerText = 'Cambiado por 3'
